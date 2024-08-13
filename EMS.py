d53b6846e73a5857614dc8133d8cb85e48474cf1
Adityasinh Sodha <work.adityasodha@gmail.com>
2024-08-13 09:45:45 +0530

    Update EMS.py

--- EMS.py
+++ EMS.py
@@ -1,5 +1,11 @@
 import tkinter as tk
-from mapping import custom_mapping
+import base64
+import json
+
+
+encoded_mapping = "eyJBIjogIjQ2ODAyNyIsICJCIjogIjkxMDM2NCIsICJDIjogIjU4MDAzMSIsICJEIjogIjI3MDQ1NiIsICJFIjogIjM5MjA3MCIsICJGIjogIjgwMDUxOSIsICJHIjogIjcyMDUzMSIsICJIIjogIjE0MDY5MCIsICJJIjogIjYzMDcyNCIsICJKIjogIjk1MDAxMiIsICJLIjogIjMwODE3NiIsICJMIjogIjU3MDM4MiIsICJNIjogIjgxMDk0NyIsICJOIjogIjIwMDUzOCIsICJPIjogIjcwMTYyNSIsICJQIjogIjM5MDQ3MCIsICJRIjogIjg1MDA2MSIsICJSIjogIjE2MDI1OSIsICJTIjogIjMwOTU2MiIsICJUIjogIjQ3MDgzNiIsICJVIjogIjkzNjAwMSIsICJWIjogIjcwMDIzNSIsICJXIjogIjQxMDU3OCIsICJYIjogIjYwODAyNSIsICJZIjogIjI4MzEwNSIsICJaIjogIjc1MDk2MCIsICIgIjogIjU0MDI2MyIsICJhIjogIjIwNDg5NiIsICJiIjogIjYxMDQ1MiIsICJjIjogIjkzNTIwMSIsICJkIjogIjQwNzYxMyIsICJlIjogIjkxNTAyNCIsICJmIjogIjIwMzc4NiIsICJnIjogIjU5ODA2MSIsICJoIjogIjE3MDM0OSIsICJpIjogIjY4NTAxOSIsICJqIjogIjQwMjM1NyIsICJrIjogIjc5MDUxMCIsICJsIjogIjMxMDIwNCIsICJtIjogIjU5MDA4MSIsICJuIjogIjIwMDYzOCIsICJvIjogIjcxMDQ5MiIsICJwIjogIjMwNjg1NyIsICJxIjogIjk4MDAxNSIsICJyIjogIjY0MDEyNSIsICJzIjogIjUzMDA5OCIsICJ0IjogIjc4MDUwMyIsICJ1IjogIjYxMDA5MiIsICJ2IjogIjQwODUxMyIsICJ3IjogIjI3OTA2MSIsICJ4IjogIjUwODMwOSIsICJ5IjogIjMwNDYxMiIsICJ6IjogIjgwMjc0NSIsICIxIjogIjYzODA5MSIsICIyIjogIjU5MTA0NyIsICIzIjogIjgyMDQwMCIsICI0IjogIjM1NzkwMiIsICI1IjogIjE5NDYwNyIsICI2IjogIjc1MDAzMSIsICI3IjogIjUwMDM4MiIsICI4IjogIjgxMDYyNSIsICI5IjogIjIzOTA4NCIsICIwIjogIjU3MDEzNiIsICIhIjogIjkyMDcwNCIsICJAIjogIjQ2MjUwMSIsICIjIjogIjcxNTM5MCIsICIkIjogIjg0NjMwMiIsICIlIjogIjM1OTAyNCIsICJeIjogIjE4MDIzNiIsICImIjogIjUwMDEyOCIsICIqIjogIjc5MzAxNSIsICIoIjogIjYyODcwNSIsICIpIjogIjQzNzE5MCIsICJfIjogIjM2MjA0MCIsICIiOiAiNTAyODAxIiwgIi0iOiAiMjA0NjM5IiwgIj0iOiAiOTEwNTgzIiwgIn4iOiAiNzgzMDE1IiwgInsiOiAiNjE5MzUwIiwgIn0iOiAiNDcwODI2IiwgIlsiOiAiMjUwOTYzIiwgIl0iOiAiODYwMzE1IiwgIjoiOiAiNjkwMjQ4IiwgIjsiOiAiMTA2Mjk0IiwgIiciOiAiOTcwMDM1IiwgIj4iOiAiNTQxMDgyIiwgIj8iOiAiMzAyODU3IiwgIjwiOiAiNDc1OTAzIiwgIiwiOiAiNjAzOTIxIiwgIi4iOiAiNTE4MzA3IiwgIi8iOiAiMjA2NDc5IiwgInwiOiAiNTA3MzkxIiwgIisiOiAiNjc0NTM0IiwgIlwiIjogIjkyODYwMyIsICJcXCI6ICIxNTczMDkiLCAiYCI6ICIxMDEwMTAifQ=="
+
+custom_mapping = json.loads(base64.b64decode(encoded_mapping).decode())
 
 def encode_text(text, mapping):
     encoded_chars = []
@@ -30,10 +36,10 @@ def on_encode(event):
 
 def on_decode():
     try:
-        clipboard_text = root.clipboard_get().strip()  
+        clipboard_text = root.clipboard_get().strip()
         if clipboard_text:
             decode_entry.delete(0, tk.END)
-            decode_entry.insert(tk.END, clipboard_text)  
+            decode_entry.insert(tk.END, clipboard_text)
             decoded_text = decode_text(clipboard_text, custom_mapping)
             decoded_entry.delete("1.0", tk.END)
             decoded_entry.insert(tk.END, decoded_text)
